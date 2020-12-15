36abee156136ca44705f150e453299dafe49704a
EmilieWong <wongemilie@gmail.com>
2020-12-15 23:06:51 +0100

    Morse code. Failing tests. Pending Jenkins configuration

---
+++ morse.py
@@ -0,0 +1,9 @@
+
+# morse.py
+# pylint: disable=missing-docstring
+
+def decode(message):
+    pass # TODO: implement the behavior!
+
+if __name__ == "__main__":
+    sentence = decode(".- .-.. .-.. / -.-- --- ..- / -. . . -.. / .. ... / -.-. --- -.. .")
