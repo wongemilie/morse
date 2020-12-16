676a77d174b1d9f5ec7250bab0eb080f6a300183
EmilieWong <wongemilie@gmail.com>
2020-12-16 11:01:43 +0100

    100% passing tests for one-word Morse decoder

--- morse.py
+++ morse.py
@@ -2,8 +2,51 @@
 # morse.py
 # pylint: disable=missing-docstring
 
+ALPHABET = {
+    '.-':   'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..':  'D',
+    '.':    'E',
+    '..-.': 'F',
+    '--.':  'G',
+    '....': 'H',
+    '..':   'I',
+    '.---': 'J',
+    '-.-':  'K',
+    '.-..': 'L',
+    '--':   'M',
+    '-.':   'N',
+    '---':  'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.':  'R',
+    '...':  'S',
+    '-':    'T',
+    '..-':  'U',
+    '...-': 'V',
+    '.--':  'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+
 def decode(message):
-    pass # TODO: implement the behavior!
+    if not message:
+        return ""
+
+    morse_message = message.split(" ")
+    converted_message = ""
+
+    print(f'Morse message : {morse_message}')
+
+    for symbol in morse_message:
+        print(f'Converting {symbol} into letter...')
+        converted_message += ALPHABET[symbol]
+
+    print(f'Converted message : {converted_message}')
+    return converted_message #''.join(converted_message)
 
 if __name__ == "__main__":
-    sentence = decode(".- .-.. .-.. / -.-- --- ..- / -. . . -.. / .. ... / -.-. --- -.. .")
+    print(decode("... --- ..."))
+    print(decode("-..-"))
